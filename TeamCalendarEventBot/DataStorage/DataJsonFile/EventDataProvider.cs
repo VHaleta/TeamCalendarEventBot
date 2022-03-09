19a335380faf2de51ce40cd285dbb7f3a184ba61
VHaleta <vhaleta.official@gmail.com>
2022-03-09 18:38:53 +0200

    Added possibility to work with event file

--- TeamCalendarEventBot/DataStorage/DataJsonFile/EventDataProvider.cs
+++ TeamCalendarEventBot/DataStorage/DataJsonFile/EventDataProvider.cs
@@ -1,16 +1,66 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamCalendarEventBot.Models;
 
 namespace TeamCalendarEventBot.DataStorage.DataJsonFile
 {
-    internal class EventDataProvider : JsonFileDataProviderBase, IEventDataProvider
+    public class EventDataProvider : JsonFileDataProviderBase, IEventDataProvider
     {
+        private const string _fileName = "general_events.dat";
+        private static object _locker = new object();
         public EventDataProvider(IFileProvider fileProvider) : base(fileProvider)
         {
             
+        }
+
+        public void AddGeneralEvent(CalendarEvent calendarEvent)
+        {
+            lock (_locker)
+            {
+                var calendarEvents = GetGeneralEvents();
+                calendarEvents.Add(calendarEvent);
+
+                calendarEvents = calendarEvents.OrderBy(x => x.Date).ToList();
+                var newDataContents = JsonConvert.SerializeObject(calendarEvents);
+                FileProvider.WriteFile(_fileName, newDataContents);
+
+            }
+        }
+
+        public void DeleteGeneralEvent(CalendarEvent calendarEvent)
+        {
+            lock (_locker)
+            {
+                var calendarEvents = GetGeneralEvents();
+                for (int i = 0; i < calendarEvents.Count; i++)
+                {
+                    if(calendarEvents[i].Id == calendarEvent.Id)
+                    {
+                        calendarEvents.RemoveAt(i);
+                    }
+                }
+                var newDataContents = JsonConvert.SerializeObject(calendarEvents);
+                FileProvider.WriteFile(_fileName, newDataContents);
+            }
+        }
+
+        public List<CalendarEvent> GetGeneralEvents()
+        {
+            lock (_locker)
+            {
+                var dataContents = FileProvider.ReadFile(_fileName);
+                if (string.IsNullOrWhiteSpace(dataContents))
+                {
+                    return new List<CalendarEvent>();
+                }
+
+                return JsonConvert.DeserializeObject<List<CalendarEvent>>(dataContents);
+
+            }
         }
     }
 }
--- TeamCalendarEventBot/DataStorage/IEventDataProvider.cs
+++ TeamCalendarEventBot/DataStorage/IEventDataProvider.cs
@@ -1,6 +1,12 @@
-﻿namespace TeamCalendarEventBot.DataStorage
+﻿using System.Collections.Generic;
+using TeamCalendarEventBot.Models;
+
+namespace TeamCalendarEventBot.DataStorage
 {
     public interface IEventDataProvider
     {
+        List<CalendarEvent> GetGeneralEvents();
+        void AddGeneralEvent(CalendarEvent calendarEvent);
+        void DeleteGeneralEvent(CalendarEvent calendarEvent);
+        }
     }
-}
