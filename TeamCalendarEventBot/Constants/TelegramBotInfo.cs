b10d1fa055263baf35682abe6ed3af39212f89ba
VHaleta <97258811+VHaleta@users.noreply.github.com>
2023-02-27 15:32:47 +0200

    clear

--- TeamCalendarEventBot/Constants/TelegramBotInfo.cs
+++ TeamCalendarEventBot/Constants/TelegramBotInfo.cs
@@ -2,7 +2,6 @@
 {
     public static class TelegramBotInfo
     {
-        public const string MainToken = "5017532709:AAGQiasjtjCgvCMJQXmm2YUznOvsbucsVbY";
-        public const string TestToken = "5601516781:AAH01eGWFGZTO_LObkgrDOV-R3fGfWer3e0";
+        public const string MainToken = "";
     }
 }
