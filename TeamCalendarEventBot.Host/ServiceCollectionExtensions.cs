31f4f56ad709dc744ee6b8f2959e9772da530345
VHaleta <vhaleta.official@gmail.com>
2024-05-18 21:57:34 +0300

    Add projects

---
+++ TeamCalendarEventBot.Host/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TeamCalendarEventBot.Host
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDalDependencies(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
